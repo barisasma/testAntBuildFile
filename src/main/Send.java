9bbef2d784eb51bc5c317a73d8ce9f3d0adc3e8f
baris asma <asma.baris@gmail.com>
2020-02-05 17:11:22 +0300

    external jars added, property file added

--- src/main/Send.java
+++ src/main/Send.java
@@ -1,11 +1,15 @@
 package main;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+
 public class Send {
-
-	public static void main(String[] args) {
-		
-		System.out.println("Test test");
-
+	
+	static Logger logger = Logger.getLogger(Send.class);
+	
+	public static void main(String[] args) throws Exception {
+		logger.info("Send test");
+		System.out.println("String Utils Test: "+ StringUtils.isEmpty("String"));
 	}
 
 }
